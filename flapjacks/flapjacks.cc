5cc8fff99c0d27648aad760e9d278721096fa22f
unknown <andy@.(none)>
2010-04-19 19:02:31 -0600

    Read input for flapjacks

--- flapjacks/flapjacks.cc
+++ flapjacks/flapjacks.cc
@@ -11,7 +11,47 @@ NOTE: A flapjack is a sweet snack made from oats and syrup.
 It is not the same as a pancake.
 */
 
+class flapstack {
+	// List of pancake sizes, bottom first
+	list<int> pancakes;
+	
+	public:
+		void add(int);
+};
+
+void flapstack::add(int size) {
+	pancakes.push_front(size);
+}
+
+void writeFlips(flapstack stack) {
+	int nextFlip;
+	while (stack.flipOne(&nextFlip))
+		cout << nextFlip << ' ';
+	cout << '0' << endl;
+}
+
+flapstack readStack(string line) {
+	// Consume pancakes
+	stringstream stackReader(line, stringstream::in);
+	
+	flapstack stack;
+	int nextSize;
+	
+	while (!stackReader.eof()) {
+		stackReader >> nextSize;
+		stack.add(nextSize);
+	}
+	
+	return stack;
+}
+
 int main() {
+	string line;
+	while (getline(cin, line))
+	{
+		flapstack current = readStack(line);
+		writeFlips(current);
+	}
 
 	return 0;
 }
