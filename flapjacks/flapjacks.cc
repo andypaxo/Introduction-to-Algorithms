ecf512bf1f44c71d3151c0226a873f89f50efdc8
unknown <andy@.(none)>
2010-04-19 18:05:07 -0600

    Files for 'stacks of flapjacks' problem.

---
+++ flapjacks/flapjacks.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <sstream>
+using namespace std;
+
+/*
+Solution for 'stacks of flapjacks'
+
+NOTE: A flapjack is a sweet snack made from oats and syrup.
+It is not the same as a pancake.
+*/
+
+int main() {
+
+	return 0;
+}
